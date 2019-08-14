d7a4d0e34a4fcd0004948c957c8622fde68d3c67
ilhamAdhim <ilhamm179@gmail.com>
2019-08-14 22:43:01 +0700

    fixed onclick button bug

--- src/Section.js
+++ src/Section.js
@@ -2,54 +2,63 @@ import React from 'react'
 import { Table, Button, Col, Container, Row } from 'react-bootstrap'
 
 export default function Section(props) {
-    // props.lampsCollection.totalSec++
-    let hour = Math.floor(props.totalSec / 3600)
-    let minute = Math.floor((props.totalSec - hour * 3600) / 60)
-    let seconds = props.totalSec - (hour * 3600 + minute * 60)
+    // props.lampsCollection.timer++
+    let hour = Math.floor(props.timer / 3600)
+    let minute = Math.floor((props.timer - hour * 3600) / 60)
+    let seconds = props.timer - (hour * 3600 + minute * 60)
 
     const turnedOff = <td style={{ backgroundColor: '' }}> Off </td>
-    const turnedOn = <td style={{ backgroundColor: 'rgb(236, 193, 1)' }}> On</td>
+    const turnedOn = <td style={{ backgroundColor: 'rgb(236, 193, 1)', color: 'black' }}> On</td>
 
-    const buttonOn = <Button onClick={()=>{
-        props.switch({
-            led:props.lamp,
-            isTurnOn: false
+    const changeStatus = () => {
+        props.switch(props.lamp, {
+            isTurnOn: !props.isTurnOn,
+            led: props.lamp,
+            totalSec: props.timer
         })
+
+        // {
+        //     led: props.lamp,
+        //     totalSec: 400,
+        //     timer: 4,
+        //     watt: 10,
+        //     isTurnOn: props.isTurnOn ? false : true}
+
+    }
+
+    const buttonOn = <Button onClick={() => {
+        changeStatus()
     }}> Turn Off</Button>
-    const buttonOff = <Button onClick={()=>{
-        props.switch({
-            led:props.lamp,
-            isTurnOn: true
-        })
+    const buttonOff = <Button onClick={() => {
+        changeStatus()
     }}> Turn On</Button>
-    
-    return (
-        <Table variant="dark" responsive >
-            <thead >
-                <tr>
-                    <Container>
-                        <Row>
-                            <Col>
-                                Lamp {props.lamp}
-                            </Col>
-                            <Col style={{ padding: '0px' }}>
-                                {props.isTurnOn ?  buttonOn : buttonOff}
-                            </Col>
-                        </Row>
-                    </Container>
-                </tr>
-            </thead>
-            <tbody >
-                <tr style={{ height: '45vh' }}>
-                    {props.isTurnOn ? turnedOn : turnedOff}
-                </tr>
 
-                <tr>
-                    <td>
-                        {hour} : {minute} : {seconds}
-                    </td>
-                </tr>
-            </tbody>
-        </Table>
+    return (
+        <div>
+            <Container>
+                <Row>
+                    <Col>
+                        Lamp {props.lamp}
+                    </Col>
+                    <Col style={{ padding: '0px' }}>
+                        {props.isTurnOn ? buttonOn : buttonOff}
+                    </Col>
+                </Row>
+            </Container>
+
+            <Table variant="dark" responsive>
+                <tbody >
+                    <tr style={{ height: '45vh' }}>
+                        {props.isTurnOn ? turnedOn : turnedOff}
+                    </tr>
+
+                    <tr>
+                        <td>
+                            {hour} : {minute} : {seconds}
+                        </td>
+                    </tr>
+                </tbody>
+            </Table>
+        </div>
     )
 }
--- src/Table.js
+++ src/Table.js
@@ -5,36 +5,40 @@ import Section from './Section'
 
 export default function TableMenu(props) {
     const syncLamp = 3
-    const [buttonLamp, setButtonLamp] = React.useState({
-    })
 
-    const lampsCollection = [{
+    const [lamp1, setLamp1] = React.useState({
         led: 1,
         totalSec: 200,
         timer: 2,
         watt: 10,
         isTurnOn: false
-    }, {
+    })
+
+    const [lamp2, setLamp2] = React.useState({
         led: 2,
         totalSec: 300,
         timer: 3,
         watt: 10,
         isTurnOn: true
-    }, {
+    })
+
+    const [lamp3, setLamp3] = React.useState({
         led: 3,
         totalSec: 400,
         timer: 4,
         watt: 10,
         isTurnOn: false
-    }]
+    })
 
-
-    const [lampsStatus, setLampStatus] = React.useState(lampsCollection)
+    const lampsCollection = [lamp1, lamp2, lamp3]
 
     changeLampStatus.bind()
 
-    function changeLampStatus(...data) {
-        setLampStatus(data)
+    function changeLampStatus(index, data) {
+        index === 1 ?
+            setLamp1(data) :
+            index === 2 ?
+                setLamp2(data) : setLamp3(data)
     }
 
     const checkLampStatus = () => { for (let index = 0; index < syncLamp; index++)readLampStatus(index) }
@@ -45,9 +49,9 @@ export default function TableMenu(props) {
         firebase.database().ref(`/leds/` + led + `/status`).on('value', function (snapshot) {
             let status = parseInt(snapshot.val());
             if (status) {
-                
+
             } else {
-               
+
             }
             // timerLamp(led, isTurnOn)
             // setTimeout(recordTimeToFirebase(led), 1000)
@@ -57,13 +61,15 @@ export default function TableMenu(props) {
         <Table striped responsive variant='dark'>
             <tbody>
                 <tr>
-                    {lampsStatus.map(lampsStatus =>
-                        <td key={lampsStatus.led}>
+                    {/* //Coba pass datanya ga usah di map  */}
+                    {lampsCollection.map(eachLamp =>
+                        <td key={eachLamp.led}>
                             <Section
+                                collection={eachLamp}
                                 switch={changeLampStatus}
-                                lamp={lampsStatus.led}
-                                timer={lampsStatus.totalSec}
-                                isTurnOn={lampsStatus.isTurnOn} />
+                                lamp={eachLamp.led}
+                                timer={eachLamp.totalSec}
+                                isTurnOn={eachLamp.isTurnOn} />
                         </td>
                     )}
                 </tr>
